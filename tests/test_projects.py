0a5677ad4d2db04eb219d6f19072a4469d69079f
David Tucker <david@tucker.name>
2020-11-11 23:56:03 -0800

    Run flake8 on setup.py and tests

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -1,1 +1,13 @@
+"""Test the projects module."""
+
+
+import subprocess
+import sys
+
 import projects
+
+
+def test_python_m():
+    """Test python -m."""
+    command = [sys.executable, "-m", projects.__name__, "--help"]
+    assert subprocess.run(command, check=False).returncode == 0
